79af0967a3b1f5ad0c821d3c06f04de5d6802a3e
GitiAzonx <silvio.gutierrez@he-arc.ch>
2016-12-01 08:46:49 +0100

    Création des classes business

---
+++ Movies/src/java/ch/hearc/ig/odi/movies/business/Movie.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.movies.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author silvio.gutierre
+ */
+public class Movie {
+    
+    private Long id;
+    private String name;
+    private String producer;
+    private List<Person> people;
+
+    public Movie() {
+        this.people = new ArrayList<>();
+    }
+
+    public Movie(Long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }   
+    
+}
---
+++ Movies/src/java/ch/hearc/ig/odi/movies/business/Person.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.movies.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author silvio.gutierre
+ */
+public class Person {
+    
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private List<Movie> movies;
+
+    public Person() {
+        this.movies = new ArrayList<>();
+    }
+
+    public Person(Long id, String firstName, String lastName) {
+        this();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+           
+}
