8512165240ed2883f0a752cda10613f125ddf2a0
GitiAzonx <silvio.gutierrez@he-arc.ch>
2016-12-08 16:44:12 +0100

    Quelques commentaires et gestion d'exceptions

--- Movies/src/java/ch/hearc/ig/odi/movies/business/Person.java
+++ Movies/src/java/ch/hearc/ig/odi/movies/business/Person.java
@@ -5,6 +5,7 @@
  */
 package ch.hearc.ig.odi.movies.business;
 
+import ch.hearc.ig.odi.movies.exception.InvalidParameterException;
 import ch.hearc.ig.odi.movies.exception.NullParameterException;
 import ch.hearc.ig.odi.movies.exception.UniqueException;
 import java.util.ArrayList;
@@ -64,22 +65,36 @@ public class Person {
         this.movies = movies;
     }
     
-    
+    /**
+     * Ajoute un film à la liste de la personne, et ajoute cette personne à la liste des personnes du film.
+     * @param movie Le film à ajouter à la liste
+     * @throws UniqueException Levée si la liste contient déjà ce film
+     * @throws NullParameterException Levée si le paramètre est null
+     */
     public void addMovie(Movie movie)throws UniqueException, NullParameterException{
         if(movie == null){
             throw new NullParameterException("Movie is null");
-        }
-        
+        }        
         if(movies.contains(movie)){
-            throw new UniqueException("Error, the movie the movie is already in the list");
-        }
-        
+            throw new UniqueException("Error, the movie is already in the list");
+        }        
         movie.addPerson(this);
         movies.add(movie);            
     }
     
-    public void removeMovie(Movie movie) throws NullParameterException{
-        movie.removePerson(this);
-        movies.remove(movie);                
+    /**
+     * Supprime le film passé en paramètre de la liste de la personne et supprime cette personne de la liste de personne de ce film
+     * @param movie Le film à supprimer de la liste
+     * @throws NullParameterException Dans le cas où le paramètre est null
+     * @throws InvalidParameterException Dans le cas où le film à supprimer n'est déjà pas dans la liste
+     */
+    public void removeMovie(Movie movie) throws NullParameterException, InvalidParameterException{
+        if(movie == null){
+            throw new NullParameterException("Movie is null");
+        }        
+        if(movies.remove(movie)){
+            throw new InvalidParameterException("That movie doesn't appear in the list");
+        }
+        movie.removePerson(this);                     
     }
 }
